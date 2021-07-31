16af6b4c74e7effa29564e491c7e8ca6d22c3e12
zdnpl <83897266+zdnpl@users.noreply.github.com>
2021-07-31 16:23:57 +0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+	{
+		numb: 1,
+		question: "Siapa yang bikin Quiz ini?",
+		answer: "Zidan",
+		options: [
+			"Gatau",
+			"Zidan",
+			"Mark burgerberg",
+			"steve jobs"
+		]
+	},
+	{
+		numb: 2,
+		question: "1 * 1 - 2 =",
+		answer: "-1",
+		options: [
+			"-1",
+			"1",
+			"0",
+			"2"
+		]
+	},
+	{
+		numb: 3,
+		question: "ayah dono punya 5 anak, januari, februari, maret, april, siapa nama anak ke 5?",
+		answer: "dono",
+		options: [
+			"april",
+			"mei",
+			"bogeng",
+			"dono"
+		]
+	},
+	{
+		numb: 4,
+		question: "siapa nama temannya budi?",
+		answer: "bayu",
+		options: [
+			"budi",
+			"nana",
+			"arif",
+			"bayu"
+		]
+	},
+	{
+		numb: 5,
+		question: "manusia lari keliatan?",
+		answer: "iya",
+		options: [
+			"iya",
+			"kakinya",
+			"ngga",
+			"cepetnya"
+		]
+	},
+];
